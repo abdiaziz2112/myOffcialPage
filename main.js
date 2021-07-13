1a41bc1c8ff1b0a2fbcbbeddf621197556f0d5c9
abdiaziz2112 <84683112+abdiaziz2112@users.noreply.github.com>
2021-07-13 10:32:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
+/* clock */
+
+
+let today = new Date();
+let hourNow = today.getHours();
+let minuteNow = today.getMinutes();
+let secondNow = today.getSeconds();
+
+var all = hourNow + ":" + minuteNow + ":" + secondNow;
+//document.write('<h2 class="time" >' + all + '</h2>');
+
+let timeNow = today.getHours();
+
+let display;
+
+
+if (timeNow > 18){
+  display = "Fiidnimo Wanaagsan !";
+}
+else if (timeNow > 12 ){
+  display = "Galab wanaagsan !";
+}
+else if (timeNow > 0 ){
+  display = "Subax wanaagsan!";
+}
+
+else{
+  display = "Welcome bro";
+}
+
+document.write('<h3 class="time">' + display + '</h3>')
+
+
+
+      
+
+
+
+
+
 /*===== MENU SHOW =====*/ 
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -63,41 +104,4 @@ sr.reveal('.contact__input',{interval: 200});
 /* books */
 sr.reveal('.note',{});
 sr.reveal('.section-title',{});
-
-/* clock */
-
-
-let today = new Date();
-let hourNow = today.getHours();
-let minuteNow = today.getMinutes();
-let secondNow = today.getSeconds();
-
-var all = hourNow + ":" + minuteNow + ":" + secondNow;
-//document.write('<h2 class="time" >' + all + '</h2>');
-
-let timeNow = today.getHours();
-
-let display;
-
-
-if (timeNow > 18){
-  display = "Fiidnimo Wanaagsan !";
-}
-else if (timeNow > 12 ){
-  display = "Galab wanaagsan !";
-}
-else if (timeNow > 0 ){
-  display = "Subax wanaagsan!";
-}
-
-else{
-  display = "Welcome bro";
-}
-
-document.write('<h3 class="time">' + display + '</h3>')
-
-
-
-      
-
 
