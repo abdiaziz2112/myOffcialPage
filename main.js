5a0750728f123044a5e4aec9ac70156028a76ad2
abdiaziz2112 <84683112+abdiaziz2112@users.noreply.github.com>
2021-07-13 10:26:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -64,5 +64,40 @@ sr.reveal('.contact__input',{interval: 200});
 sr.reveal('.note',{});
 sr.reveal('.section-title',{});
 
+/* clock */
+
+
+let today = new Date();
+let hourNow = today.getHours();
+let minuteNow = today.getMinutes();
+let secondNow = today.getSeconds();
+
+var all = hourNow + ":" + minuteNow + ":" + secondNow;
+//document.write('<h2 class="time" >' + all + '</h2>');
+
+let timeNow = today.getHours();
+
+let display;
+
+
+if (timeNow > 18){
+  display = "Fiidnimo Wanaagsan !";
+}
+else if (timeNow > 12 ){
+  display = "Galab wanaagsan !";
+}
+else if (timeNow > 0 ){
+  display = "Subax wanaagsan!";
+}
+
+else{
+  display = "Welcome bro";
+}
+
+document.write('<h3 class="time">' + display + '</h3>')
+
+
+
+      
 
 
